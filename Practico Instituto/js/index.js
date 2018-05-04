53aaa60a70dc675f7c57fc9ed963cc4e1ddb7c03
Emiliana Korolkoff <emipi7@gmail.com>
2018-05-04 05:59:41 -0300

    Modal
    uní los dos archivos js, hice el modal (required no funciona), en js hice a función persona y el botón de guardado pero el prototype no funciona

--- Practico Instituto/js/index.js
+++ Practico Instituto/js/index.js
@@ -23,3 +23,57 @@ $(document).mouseup(function (e)
         $('#loginform').removeClass('green');
     }
 });
+
+$(document).ready(function(){
+  $("#consultar").click(function(){
+      $("#cuerpo").html("");
+      $.ajax({
+          url:"http://www.scaggiano.com.uy/json.js",
+          success: function(data){
+              var alumnos = JSON.parse(data);
+              $("#success").css("display", "block");
+              $("#tabla").css("display", "table");
+
+              for (var i=0; i<alumnos.length;i++) {
+                  var name = alumnos[i].nombre;
+                  var surname = alumnos[i].apellido;
+                  var mail = alumnos[i].email;
+                  var age = alumnos[i].edad;
+                  var pic = alumnos[i].foto;
+                  $("#cuerpo").append("<tr><td>" + name + "</td><td>" + surname + "</td><td>" + mail + "</td><td>" + age + "</td><td>" + "<img width=50px src=" + pic + ">" + "</td><td>" + "</tr>") 
+              };
+          },
+          error: function(){
+              $("#error").css("display", "block");
+          },
+          datatype:"jsonp",
+      });
+  });
+});
+
+function Persona (nom,apell,mail,ed,img) {
+    this.nomnbre = "";
+    this.apellido = "";
+    this.email = "";
+    this.edad = 0;
+    this.imagen = "";    
+};
+
+
+
+Persona.prototype.newTableRow = function () {
+    alert ("Hola, mi nombre es " + this.nombre)
+};
+
+$(document).ready(function () {
+  $("#btnGuardar").click(function() {
+    var nom = $("#nombre").val();
+    var apell = $("#apellido").val();
+    var mail = $("#email");
+    var ed = $("#edad").val();
+    var img = $("#imagen").val();
+
+    var persona1 = new Persona(nom, apell, mail, ed, img);
+    persona1.newTableRow()
+  });
+});
