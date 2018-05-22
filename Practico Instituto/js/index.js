ca8af4004c0d36b808f7a25ccc3c431fc759d110
Emiliana Korolkoff <emipi7@gmail.com>
2018-05-22 18:03:27 -0300

    Correcciones
    Correcciones

--- Practico Instituto/js/index.js
+++ Practico Instituto/js/index.js
@@ -10,7 +10,7 @@ $('#loginform').click(function(){
   $(this).toggleClass('green');
 });
 
-
+var alumnos= [];
 
 $(document).mouseup(function (e)
 {
@@ -24,24 +24,27 @@ $(document).mouseup(function (e)
     }
 });
 
+function mostrarTabla(){
+          $("#cuerpo").html("");
+    for (var i=0; i<alumnos.length;i++) {
+        var name = alumnos[i].nombre;
+        var surname = alumnos[i].apellido;
+        var mail = alumnos[i].email;
+        var age = alumnos[i].edad;
+        var pic = alumnos[i].foto;
+        $("#cuerpo").append("<tr><td>" + name + "</td><td>" + surname + "</td><td>" + mail + "</td><td>" + age + "</td><td>" + "<img width=50px src=" + pic + ">" + "</td><td>" + "</tr>") 
+    };
+}
+
 $(document).ready(function(){
   $("#consultar").click(function(){
-      $("#cuerpo").html("");
       $.ajax({
-          url:"http://www.scaggiano.com.uy/json.js",
+          url:"https://www.scaggiano.com.uy/json.js",
           success: function(data){
-              var alumnos = JSON.parse(data);
-              $("#success").css("display", "block");
-              $("#tabla").css("display", "table");
-
-              for (var i=0; i<alumnos.length;i++) {
-                  var name = alumnos[i].nombre;
-                  var surname = alumnos[i].apellido;
-                  var mail = alumnos[i].email;
-                  var age = alumnos[i].edad;
-                  var pic = alumnos[i].foto;
-                  $("#cuerpo").append("<tr><td>" + name + "</td><td>" + surname + "</td><td>" + mail + "</td><td>" + age + "</td><td>" + "<img width=50px src=" + pic + ">" + "</td><td>" + "</tr>") 
-              };
+            alumnos = JSON.parse(data);
+            $("#success").css("display", "block");
+            $("#tabla").css("display", "table");
+            mostrarTabla();
           },
           error: function(){
               $("#error").css("display", "block");
@@ -51,29 +54,35 @@ $(document).ready(function(){
   });
 });
 
-function Persona (nom,apell,mail,ed,img) {
-    this.nomnbre = "";
+function Persona(primNombre) {
+    this.nombre = primNombre;
     this.apellido = "";
     this.email = "";
     this.edad = 0;
-    this.imagen = "";    
+    this.foto = "";    
 };
 
-
-
-Persona.prototype.newTableRow = function () {
-    alert ("Hola, mi nombre es " + this.nombre)
+Persona.prototype.hola = function(){
+    alert(this.nombre + this.apellido + this.edad + this.email + this.imagen)
 };
 
 $(document).ready(function () {
   $("#btnGuardar").click(function() {
     var nom = $("#nombre").val();
     var apell = $("#apellido").val();
-    var mail = $("#email");
+    var mail = $("#email").val();
     var ed = $("#edad").val();
     var img = $("#imagen").val();
 
-    var persona1 = new Persona(nom, apell, mail, ed, img);
-    persona1.newTableRow()
+    var persona1 = new Persona(nom);
+    persona1.apellido = apell;
+    persona1.email = mail;
+    persona1.edad = ed;
+    persona1.imagen = img;
+
+    alumnos.push(persona1);
+    mostrarTabla();
+    persona1.hola();  
+    $('#myModal').modal('hide');
   });
 });
